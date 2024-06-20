9fae80a41b80f9826bba2c42b02247bbef8c889a
karen07 <karen0734@gmail.com>
2024-06-20 11:18:56 +0300

    Formatting

---
+++ img/mkcidata.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mkisofs -output cidata.iso -volid cidata -joliet -rock meta-data user-data vendor-data
--- mkcidata.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mkisofs -output cidata.iso -volid cidata -joliet -rock img/meta-data img/user-data img/vendor-data
